7155e9ad6d6a602da4388855ceca5214ecc8994c
Sasha <oleksandr.lysyi@gmail.com>
2015-01-28 04:41:47 +0200

    all header elements

--- app/scripts/pages/home/main.js
+++ app/scripts/pages/home/main.js
@@ -1,14 +1,32 @@
 'use strict';
 
-console.log('test1');
-
 $( '.header-menu-item .open' ).click(function(e) {
   e.preventDefault();
   $(this).next().slideToggle();
 });
 
-$( '.header-menu-ico' ).click(function() {
-  $( '.header-menu' ).slideToggle();
+$('.js-main-slider').bxSlider({
+  responsive: true,
+  controls: false,
+  auto: true,
+  autoDelay: 100
 });
 
-$('.main-slider').bxSlider();
+$('.js-page-slider').bxSlider({
+  responsive: true,
+  controls: false,
+  slideWidth: 290,
+  minSlides: 1,
+  maxSlides: 3,
+  slideMargin: 20,
+  hideControlOnEnd: true
+});
+
+// var slider = $('.js-page-slider').bxSlider();
+// slider.goToNextSlide();
+
+if($(window).width() <= 500) {
+  $( '.header-menu-ico' ).click(function() {
+    $( '.js-header-menu' ).slideToggle();
+  });
+} 
